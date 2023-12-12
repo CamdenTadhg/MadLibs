a5bde75e3b443635b5a2a62f058b01107eba2266
camdentadhg <camdent@gmail.com>
2023-12-12 12:47:52 -0600

    helper text

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 selection = ''
+helperText = {'adjective': 'Describing word. Example: colorful', 'adverb': 'word describing how you do something. Often ends in "ly". Example: quickly','body_part': 'Example: toe', 'building': 'Example: library', 'color': 'Example: blue', 'comparative_adjective': 'describing word ending in "er". Example: faster', 'gerund': 'Verb ending in "ing". Example: running', 'living_thing': 'Example: dog', 'name': 'Example: Austin', 'noun': 'Person, place, or thing. Example: rose', 'past_tense_verb': 'action word that already happend. Often ends in "ed". Example: walked', 'place': 'Example: town', 'plural_noun': 'More than one person, place, or thing. Example: roses', 'sound': 'Example: screech', 'verb': 'action word. Example: run'}
+
+print(helperText['adjective'])
+
 
 @app.route('/')
 def select_story():
@@ -18,8 +22,9 @@ def select_story():
 @app.route('/form')
 def collect_words():
     """display a form to gather madlib input words"""
+    print(helperText['adjective'])
     selection = request.args["stories"]
-    return render_template("form.html", story=eval(selection))
+    return render_template("form.html", story=eval(selection), helperText=helperText)
 
 @app.route('/story')
 def show_story():
@@ -29,10 +34,9 @@ def show_story():
     return render_template("story.html", story=output_story)
 
 
-#give option to select one of the five stories
-#add styles
-#validate inputs
+#validate inputs and enter notes for word types
 #create route for new story page
 #create links to new story page
 #create form for entering new story
+#give option to select one of the five stories
 #add new story to story list on input
--- static/app.js
+++ static/app.js
@@ -3,10 +3,8 @@ $promptInputs = $('input');
 $promptLabels = $('label');
 
 counter=0
-helperText = {'adjective': 'Describing word. Example: colorful', 'adverb': 'word describing how you do something. Often ends in "ly". Example: quickly','body_part': 'Example: toe', 'building': 'Example: library', 'color': 'Example: blue', 'comparative_adjective': 'describing word ending in "er". Example: faster', 'gerund': 'Verb ending in "ing". Example: running', 'living_thing': 'Example: dog', 'name': 'Example: Austin', 'noun': 'Person, place, or thing. Example: rose', 'past_tense_verb': 'action word that already happend. Often ends in "ed". Example: walked', 'place': 'Example: town', 'plural_noun': 'More than one person, place, or thing. Example: roses', 'sound': 'Example: screech', 'verb': 'action word. Example: run'}
 
 $promptForm.on('submit', validate_form);
-$promptLabels.on('mouseover', show_helper_text);
 
 function validate_form(event) {
     originalEvent = event;
@@ -25,12 +23,3 @@ function validate_form(event) {
         $(this).trigger(originalEvent);
     }
 }
-
-// function show_helper_text(event){
-//     console.log('entering show helper text');
-//     for (let [key, value] of Object.entries(helperText)){
-//         if (event.target.innerText.startsWith(key)) {
-//             event.target.
-//         }
-//     }
-// }
